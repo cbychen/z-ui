395d3eb04b7518881bb6aec0947350fcd4fe83c3
chenboyu <15506147252@163.com>
2024-03-08 21:22:00 +0800

    feat: button color constants

---
+++ components/contants.ts
@@ -0,0 +1,109 @@
+
+export const colorPrimary = {
+  'base': '#006aff',
+  'light': {
+    // base
+    '1': '#e8f5ff',
+    '2': '#a3d6ff',
+    // disabled
+    '3': '#8bc7ff',
+    '4': '#5dabff',
+    // hover
+    '5': '#2e8cff',
+    // normal
+    '6': '#006aff',
+    // click
+    '7': '#0051d2',
+    '8': '#003aa6',
+    '9': '#002679',
+    '10': '#00164d',
+  } 
+}
+export const colorSuccess ={
+
+  'base': '#30bf78',
+  'light': {
+    // base
+    '1': '#e8fff0',
+    '2': '#bcf2d0',
+    // disabled
+    '3': '#94e5b5',
+    '4': '#70d99d',
+    // hover
+    '5': '#4ecc89',
+    // normal
+    '6': '#30bf78',
+    // click
+    '7': '#1fa265',
+    '8': '#118653',
+    '9': '#076942',
+    '10': '#004d30',
+  },
+}
+
+export const colorWarning ={
+  
+  'base': '#ffaa00',
+  'light': {
+    // base
+    '1': '#fff7e8',
+    '2': '#ffe4ba',
+    // disabled
+    '3': '#ffd88b',
+    '4': '#ffc95d',
+    // hover
+    '5': '#ffbb33',
+    // normal
+    '6': '#ffaa00',
+    // click
+    '7': '#d28500',
+    '8': '#a65e00',
+    '9': '#793800',
+    '10': '#4d1a00',
+  },
+}
+
+export const colorError ={
+  'base': #f46144,
+  'light':{ 
+    // base
+    '1': '#fff0e8',
+    '2': '#fdd7c6',
+    // disabled
+    '3': '#fbbca5',
+    '4': '#f89f84',
+    // hover
+    '5': '#f68164',
+    // normal
+    '6': '#f46144',
+    // click
+    '7': '#ca402a',
+    '8': '#a02416',
+    '9': '#761008',
+    '10': '#4d0300',
+  }
+}
+
+export const colorInfo ={
+  'base': '#1d2129',
+  'light': {
+    // white
+    '1': '#ffffff',
+    // base
+    '2': '#f2f3f5',
+    // disabled
+    '3': '#e5e7eb',
+    // border
+    '4': '#d7dbe0',
+    '5': '#c2c6d1',
+    '6': '#a9b2c2',
+    // icon
+    '7': '#7a8699',
+    '8': '#4b5669',
+    '9': '#2e3542',
+    // black
+    '10': '#1d2129',
+  }
+}
+
+
